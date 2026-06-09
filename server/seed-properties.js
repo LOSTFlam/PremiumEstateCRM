acb74b70bc7fd2561c20d00083737893d42571b7
LOSTFlam <cahek1234500000@gmail.com>
2026-06-09 14:23:47 +0300

    fix: replace remaining bcrypt requires with bcryptjs (user.js, seed scripts)

--- server/seed-properties.js
+++ server/seed-properties.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const User = require("./model/schema/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const path = require("path");
 const { connectWithFallback } = require("./utils/mongoConnect");
 
